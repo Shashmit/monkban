9841c52efbca0b3aeb24643c7ef4d1a2e797f661
Shashmit kumar <shaashmit@gmail.com>
2023-11-09 09:18:28 +0530

    changes to the reload()

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,7 @@ import { LoadingButton } from "@mui/lab";
 import { Box } from "@mui/material";
 import { useDispatch } from "react-redux";
 import { setBoards } from "../redux/features/boardSlice";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, redirect } from "react-router-dom";
 import boardApi from "../api/boardApi";
 import { useState } from "react";
 
@@ -10,12 +10,16 @@ const Home = () => {
   const navigate = useNavigate();
   const dispatch = useDispatch();
   const [loading, setLoading] = useState(false);
-  
+
   const createBoard = async () => {
     try {
       const res = await boardApi.create();
+      if (res) {
+        navigate(`/boards/${res._id}`);
+        window.location.reload();
+      }
+
       dispatch(setBoards(res));
-      navigate(`/boards/${res._id}`);
     } catch (err) {
       alert(err.message);
     } finally {
